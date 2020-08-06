fc949d9f8b061ca2c666e0fc99f2e609da3f08be
rlaplaud <ronan.laplaud@gfi.world>
2020-08-06 16:53:18 +0200

    apollo init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import {ApolloClient, ApolloProvider, HttpLink, InMemoryCache} from "@apollo/client";
 
 function App() {
+
+  const headers = {
+    'x-hasura-admin-secret': process.env.REACT_APP_APOLLO_ADMIN_KEY
+  };
+  const httpLink = new HttpLink({
+    uri: process.env.REACT_APP_APOLLO_URL,
+    headers
+  });
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+        </div>
+      </ApolloProvider>
   );
 }
 

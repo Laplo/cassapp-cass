78a5b4515e822a3b821cea721c8199c82661dd02
laplijr <ronanlaplaud4@gmail.com>
2020-09-01 15:51:55 +0200

    env variables for keycloak

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 export const keycloak = new Keycloak({
-    clientId: process.env.KEYCLOAK_CLIENT_ID,
-    realm: process.env.KEYCLOAK_REALM,
-    url: process.env.KEYCLOAK_URL
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM,
+    url: process.env.REACT_APP_KEYCLOAK_URL
 });
 
 export const keycloakProviderInitConfig = {
